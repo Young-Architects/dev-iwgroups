56e16df36d75dbd39849d4f97d9114406505c626
webdevraaj-web <webdev.raaj@gmail.com>
2026-03-04 11:31:41 +0530

    updated code

--- src/lib/wordpress.ts
+++ src/lib/wordpress.ts
@@ -1,9 +1,9 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_PROXY_URL;
 
 export const fetchPageBySlug = async (slug: string) => {
   try {
     const res = await fetch(
-      `${BASE_URL}/api/v1/home`,
+      `/api/v1/home`,
       { cache: 'no-store' }
     );
     const data = await res.json();
